62d870a333f6480431cb95e2db3b7e583f00eee6
Chesebro11 <84700256+Chesebro11@users.noreply.github.com>
2021-09-19 20:33:34 -0700

    save cities to localstorage

--- assets/script.js
+++ assets/script.js
@@ -27,7 +27,7 @@ var formSubmitHandler = function(event){
 
 // save recent cearches
 var saveSearch = function(){
-
+    localStorage.setItem('cities', json.stringify(cities));
 };
 
 //api call to get weather
