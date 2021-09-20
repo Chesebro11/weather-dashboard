f0ca3b92bbe603313b8cf009efbf6c1139cc3011
Chesebro11 <84700256+Chesebro11@users.noreply.github.com>
2021-09-19 19:13:55 -0700

    improved search box restructure html JS variables

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,13 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+var btn = document.querySelector('#btn')
 
+var containerForecastCity = document.querySelector('#forecastcity');
+
+var containerSearchedCity = document.querySelector('#searchedcity');
+
+var containerRecentCity = document.querySelector('#recentcity');
 // Fetch request grab data pass data on
 
 // need button function that saves the recently searched city
